e44f6360ccdac8d93e426ed9bd9817d237209e79
Beniamin Costandin <>
2021-11-05 16:54:55 +0200

    remodel build

--- chapter1/03_Building_and_linking_static_and_shared_libraries/cxx-objlib-example/build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set	build_folder_name=bin
-
-if not exist %build_folder_name% mkdir %build_folder_name%
-cmake -G "MinGW Makefiles" -DCMAKE_CXX_COMPILER=g++ -B %build_folder_name%  -S ../cxx-objlib-example
-mingw32-make -C %build_folder_name%
--- chapter1/recipe-01/cxx-example/build.bat
+++ chapter1/recipe-01/cxx-example/build.bat
@@ -1,7 +1,15 @@
 @echo off
+set PATH_TO_SCRIPT=%~dp0
+set	BUILD_FOLDER_NAME=bin
+set PATH_TO_MINGW=%PATH_TO_SCRIPT%../../../tool/mingw64/bin
+set PATH_TO_MAKE=%PATH_TO_MINGW%/mingw32-make.exe
+set PATH_TO_C_COMPILER=%PATH_TO_MINGW%/gcc.exe
+set PATH_TO_CXX_COMPILER=%PATH_TO_MINGW%/g++.exe
+set PATH_TO_CMAKE=%PATH_TO_SCRIPT%../../../tool/cmake/3.21.4/bin/cmake.exe
 
-set	build_folder_name=bin
+if not exist %BUILD_FOLDER_NAME% mkdir %BUILD_FOLDER_NAME%
+%PATH_TO_CMAKE% -G "MinGW Makefiles" -D "CMAKE_MAKE_PROGRAM:PATH=%PATH_TO_MAKE%" "CMAKE_CXX_COMPILER=:PATH=%PATH_TO_CXX_COMPILER%" -B %BUILD_FOLDER_NAME%  -S ../cxx-example
 
-if not exist %build_folder_name% mkdir %build_folder_name%
-cmake -G "MinGW Makefiles" -DCMAKE_CXX_COMPILER=g++ -B %build_folder_name%  -S ../cxx-example
-mingw32-make -C %build_folder_name%
+%PATH_TO_MAKE% -C %BUILD_FOLDER_NAME%
+
+rem D:\exterior\projects\cmake_repo\tool\cmake\3.21.4\bin\cmake.exe -G "MinGW Makefiles" -D CMAKE_C_COMPILER=gcc -B %build_folder_name%  -S ../c-example
